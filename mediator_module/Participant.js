5ec825e1badea76b1d302ee3c020a99247d4cd1c
Igor Kolganov <igor3igor@rambler.ru>
2025-12-03 14:42:11 -0500

    split mediator pattern by modules

---
+++ mediator_module/Participant.js
@@ -0,0 +1,15 @@
+class Participant{
+    constructor(name){
+        this.name = name
+        this.chatroom = null
+    }
+
+    send(message, to){
+        this.chatroom.send(message, this, to)
+    }
+
+    receive(message, from){
+        console.log(from.name + " to " + this.name + ": " + message)
+    }
+}
+export {Participant}
---
+++ mediator_module/main.js
@@ -0,0 +1,17 @@
+import { ChatRoom } from "./Mediator.js"
+import { Participant } from "./Participant.js";
+
+let chatroom = new ChatRoom()
+
+let mike = new Participant('Mike')
+let jimm = new Participant('Jimm')
+let jane = new Participant('Jane')
+
+chatroom.register(mike)
+chatroom.register(jimm)
+chatroom.register(jane)
+
+mike.send("Hello Jane! Do you go out with me?")
+mike.send("Something I need to descuss with you.")
+jimm.send("Hey, you send to all that messages.", mike)
+jane.send("Common Mike!")
