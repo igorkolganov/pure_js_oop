635c3b532b17f71c9109a664d9c02f3a16ffa732
Igor Kolganov <igor3igor@rambler.ru>
2025-12-05 12:50:13 -0500

    write logic of mediator

--- mediator_node/index.js
+++ mediator_node/index.js
@@ -1,3 +1,21 @@
-import fs from 'fs';
+import fs from 'fs'
+import os from 'os'
 
-console.log('hello')
+import {ChatRoom} from './src/mediator/Mediator.js'
+import {Participant} from './src/Participant.js'
+
+let chatRoom = new ChatRoom()
+
+let mike = new Participant('Mike', 'chats/mike.txt', fs)
+let jimm = new Participant('Jimm', 'chats/jimm.txt', fs)
+let jane = new Participant('Jane', 'chats/jane.txt', fs)
+
+chatRoom.register(mike)
+chatRoom.register(jimm)
+chatRoom.register(jane)
+
+mike.send("Hello, Jane! Do you go out with me?" + os.EOL)
+mike.send("Something I need to discuss with you." + os.EOL)
+jimm.send("Hey, you send to all that message." + os.EOL, mike)
+jane.send("Common Mike!" + os.EOL)
+
